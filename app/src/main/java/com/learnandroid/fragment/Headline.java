3444a0ae6739a98359a07f293b4d281bf5174a6a
Malik Al Ichsan <malik.al.ichsan@gmail.com>
2018-01-10 09:43:52 +0700

    article & headline activity

--- app/src/main/java/com/learnandroid/fragment/Headline.java
+++ app/src/main/java/com/learnandroid/fragment/Headline.java
@@ -1,13 +1,17 @@
 package com.learnandroid.fragment;
 
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class Headline extends AppCompatActivity {
-
+public class Headline extends Fragment {
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_headline);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_headline, container, false);
     }
 }
